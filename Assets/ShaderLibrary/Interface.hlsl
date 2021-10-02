0908009d4e7709bb9df4bbecdae9c6422b918e19
572005238 <572005238@users.noreply.github.com>
2021-10-03 01:09:51 +0800

    chapter 2

---
+++ Assets/ShaderLibrary/Interface.hlsl
@@ -0,0 +1,12 @@
+﻿#ifndef CRP_INTERFACE_INCLUDED
+#define CRP_INTERFACE_INCLUDED
+
+#include "UnityInput.hlsl"
+
+float3 MeshCenterWS()
+{
+    float3 center = mul(unity_ObjectToWorld , float(0, 0, 0, 1)).xyz;
+    return center;
+}
+
+#endif
