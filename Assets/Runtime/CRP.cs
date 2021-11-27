0c4b3ec4a760450dfc1004df64da098b7dda6b5f
572005238 <572005238@users.noreply.github.com>
2021-11-27 14:51:10 +0800

    非平行光的烘培, 涉及到衰减问题,需要自定义,否则会使用u你与默认的衰减方式造成错误. 因为平行光永远不衰减,所以不存在这个问题

--- Assets/Runtime/CRP.cs
+++ Assets/Runtime/CRP.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.Rendering;
+using LightType = UnityEngine.LightType;
 
 namespace CignalRP {
-    public class CRP : RenderPipeline {
+    public partial class CRP : RenderPipeline {
         private CameraRenderer cameraRenderer = new CameraRenderer();
 
         private bool useDynamicBatching;
@@ -12,7 +15,7 @@ namespace CignalRP {
         private PostProcessSettings postProcessSettings;
         private bool allowHDR;
 
-        public CRP(bool useDynamicBatching, bool useGPUInstancing, bool useSRPBatcher, ShadowSettings shadowSettings, 
+        public CRP(bool useDynamicBatching, bool useGPUInstancing, bool useSRPBatcher, ShadowSettings shadowSettings,
             PostProcessSettings postProcessSettings, bool allowHDR) {
             this.useDynamicBatching = useDynamicBatching;
             this.useGPUInstancing = useGPUInstancing;
@@ -22,13 +25,71 @@ namespace CignalRP {
 
             GraphicsSettings.useScriptableRenderPipelineBatching = useSRPBatcher;
             GraphicsSettings.lightsUseLinearIntensity = true;
+
+            InitForEditor();
         }
 
         protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
             for (int i = 0, length = cameras.Length; i < length; ++i) {
-                cameraRenderer.Render(ref context, cameras[i], useDynamicBatching, 
+                cameraRenderer.Render(ref context, cameras[i], useDynamicBatching,
                     useGPUInstancing, shadowSettings, postProcessSettings, allowHDR);
             }
         }
+    }
+
+    //#if UNITY_EDITOR
+    public partial class CRP : RenderPipeline {
+        partial void InitForEditor();
+
+#if UNITY_EDITOR
+		partial void InitForEditor () {
+			Lightmapping.SetDelegate(lightsDelegate);
+		}
+
+		protected override void Dispose (bool disposing) {
+			base.Dispose(disposing);
+			Lightmapping.ResetDelegate();
+		}
+
+		// 烘培delegate,因为点光源等直接bake会明显过亮，是因为使用了builtin的烘培形式，所以需要手动告知Unity
+		// todo 方向光不存在这个问题? 不存在,因为方向光永远不衰减!!!
+		static Lightmapping.RequestLightsDelegate lightsDelegate =
+			(Light[] lights, NativeArray<LightDataGI> output) => {
+				var lightData = new LightDataGI();
+				for (int i = 0; i < lights.Length; i++) {
+					Light light = lights[i];
+					switch (light.type) {
+						case LightType.Directional:
+							var directionalLight = new DirectionalLight();
+							LightmapperUtils.Extract(light, ref directionalLight);
+							lightData.Init(ref directionalLight);
+							break;
+						case LightType.Point:
+							var pointLight = new PointLight();
+							LightmapperUtils.Extract(light, ref pointLight);
+							lightData.Init(ref pointLight);
+							break;
+						case LightType.Spot:
+							var spotLight = new SpotLight();
+							LightmapperUtils.Extract(light, ref spotLight);
+							lightData.Init(ref spotLight);
+							break;
+						case LightType.Area:
+							var rectangleLight = new RectangleLight();
+							LightmapperUtils.Extract(light, ref rectangleLight);
+							rectangleLight.mode = LightMode.Baked;
+							lightData.Init(ref rectangleLight);
+							break;
+						default:
+							lightData.InitNoBake(light.GetInstanceID());
+							break;
+					}
+
+					lightData.falloff = FalloffType.InverseSquared;
+					output[i] = lightData;
+				}
+			};
+
+#endif
     }
 }
